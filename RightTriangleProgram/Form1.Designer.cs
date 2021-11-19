76b948e5e6c8a1ecaa20747f5a2db3d7af1a62d0
Christopher McDaniel <w10131827@usm.edu>
2021-11-19 08:50:00 -0600

    Add project files.

---
+++ RightTriangleProgram/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace RightTriangleProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtHypot = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(273, 79);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 585);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(592, 672);
+            this.txtB.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(85, 30);
+            this.txtB.TabIndex = 1;
+            this.txtB.TextChanged += new System.EventHandler(this.txtB_TextChanged);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(175, 397);
+            this.txtA.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(85, 30);
+            this.txtA.TabIndex = 2;
+            this.txtA.TextChanged += new System.EventHandler(this.txtA_TextChanged);
+            // 
+            // txtHypot
+            // 
+            this.txtHypot.Location = new System.Drawing.Point(682, 320);
+            this.txtHypot.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtHypot.Name = "txtHypot";
+            this.txtHypot.ReadOnly = true;
+            this.txtHypot.Size = new System.Drawing.Size(85, 30);
+            this.txtHypot.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 370);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(622, 711);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(713, 293);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "C";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 772);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHypot);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtHypot;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RightTriangleProgram/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RightTriangleProgram
+{
+    public partial class Form1 : Form
+    {
+        double a = 0;
+        double b = 0;
+        double c = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtA.Text = a.ToString();
+            txtB.Text = b.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtA_TextChanged(object sender, EventArgs e)
+        {
+            updateHypotenuse();
+        }
+
+        public void updateHypotenuse()
+        {
+            if(txtA.Text != "" && txtB.Text != "")
+            {
+                try
+                {
+                    a = Convert.ToDouble(txtA.Text);
+                    b = Convert.ToDouble(txtB.Text);
+
+                    c = Math.Sqrt(a * a + b * b);
+
+                    txtHypot.Text = c.ToString();
+                }
+                catch(Exception)
+                {
+                    txtHypot.Text = "Error";
+                }
+            }
+            else
+            {
+                txtHypot.Text = "N\\A";
+            }
+        }
+
+        private void txtB_TextChanged(object sender, EventArgs e)
+        {
+            updateHypotenuse();
+        }
+    }
+}
